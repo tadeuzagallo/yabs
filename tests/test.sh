c0254beadb4a7fcbcf0b0ef42ac3a10e127e0602
Tadeu Zagallo <tadeuzagallo@gmail.com>
2015-01-18 00:50:56 -0200

    Fix tests and test .yabsrc

--- tests/test.sh
+++ tests/test.sh
@@ -1,19 +1,23 @@
 types=(patch minor major)
-
-mkdir -p src
-
-trim() {
-  echo $1 | sed -e 's/^ *//g' -e 's/ *$//'
-}
-
+tests_count=0
 errors_count=0
 errors_output=""
 
+trim() {
+  $1
+}
+
+check() {
+  echo "$(cat $1 | sed -e 's/^ *//' -e 's/ *$//')" | md5
+}
+
 assert() {
-  if [ "$(trim $(cat $1))" != "$(trim $(cat $2))" ]; then
+  ((tests_count+=1))
+
+  if [ "$(check $1)" != "$(check $2)" ]; then
     printf 'F'
 
-    errors_count=($errors_count + 1)
+    ((errors_count+=1))
     errors_output="$errors_output
 \`$1\` == \`$2\`\n
 diff: $(diff $2 $1)\n\n"
@@ -22,20 +26,45 @@ diff: $(diff $2 $1)\n\n"
   fi
 }
 
-for type in patch minor major; do
+do_assert() {
+  for ext in json plist version; do
+    assert ./src/test.$ext ./expect/$1/test.$ext
+  done
+}
+
+copy() {
   cp -r ./fixtures/* ./src
+}
+
+run(){
+mkdir -p src
+
+for type in patch minor major; do
+  copy
 
   node ../yabs.js $type -f src/test.json -k version
   node ../yabs.js $type -f src/test.plist -k version
   node ../yabs.js $type -f src/test.version -r r/\$version/
 
-  for ext in json plist version; do
-    assert ./src/test.$ext ./expect/$type/test.$ext
-  done
+  do_assert $type
+
+  copy
+
+  node ../yabs $type
+
+  do_assert $type
 done
 
 echo "\n"
+succeeded=$(($tests_count - $errors_count))
 echo $errors_output
-echo $errors_count tests failed...
+
+echo $succeeded passing
+if [ $errors_count -gt 0 ]; then
+  echo $errors_count failing
+fi
 
 rm -rf ./src
+}
+
+time run
